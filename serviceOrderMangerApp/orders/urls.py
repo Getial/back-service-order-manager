46e3c9da57131ec542fb7d776d7f970fdd11ee8e
Felipe Getial <bfgetial@gmail.com>
2024-01-15 23:41:40 -0500

    setting url.py

--- serviceOrderMangerApp/orders/urls.py
+++ serviceOrderMangerApp/orders/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from .views import BrandViewSet, CategoryViewSet, ReferenceViewSet, ClientViewSet, UserViewSet, OrderViewSet, EvidenceViewSet
 from rest_framework.routers import DefaultRouter
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
 
 router = DefaultRouter()
 router.register('brands', BrandViewSet, basename='brands')
@@ -16,3 +18,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('docs/', include_docs_urls(title="Order API")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
